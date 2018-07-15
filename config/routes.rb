64dfd735d1e75c4d51607ec08f7a99d191ea76a5
MiguelOlayaRamirez <yamse20@yahoo.es>
2018-07-14 20:32:42 -0500

    Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :profiles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "users" => "users#index" # Crea un alias y con la asignación indico controlador y acción
+  post "users/login" #Indico directo el controlador y la acción
+  #Todos los controladores deben tener una ruta aquí de acuerdo al methodo que usen. get, post, delete, etc
+  
+  #Una carga la vista y la otra envía los datos
+  post "users/save_register"
+  get "users/register"
 end
+#rails g scaffold Profile id:integer Descripcion:string
